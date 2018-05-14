8b7edf14ca66d4861f1bf96c87ecf385bc9f2f7b
Ledio Bode <l.bode515@gmail.com>
2018-05-14 00:05:51 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,13 @@
-def my_collect(array)
+def my_collect(collection)
   i = 0
   
-  result = []
+  array = []
 
-  while i < array.size
-    block_given? # optional
-    result << yield(array[i]) # if block, then yield to block # also need to pass argument(s) to yield, otherwise block is operating on nil
+  while i < collection.size
+    block_given? 
+    array << yield(collection[i]) 
     i += 1
   end
-result
+array
 end
 
